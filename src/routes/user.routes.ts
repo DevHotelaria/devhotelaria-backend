1a55c72bc988782552424234e7279ee1171981bf
pedrolibas <102977943+pedrolibas@users.noreply.github.com>
2023-11-09 17:10:29 -0300

    feat: create middleware admin verification

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import {
     createUserController,
-} from '../controllers/user.controller'
+} from '../controllers/user.controller';
+import verifyAuthAdminMiddleware from '../middleware/verifyAuthAdmin.middleware';
 
 const userRoutes = Router();
 
-userRoutes.post('', createUserController);
+userRoutes.post('', verifyAuthAdminMiddleware, createUserController);
 
 export default userRoutes;
