ca75fc438f696486bd084f02fc59f32cf41d7479
pedrolibas <102977943+pedrolibas@users.noreply.github.com>
2023-11-10 08:44:39 -0300

    feat: add route user update

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import {
     createUserController,
+    updateUserController
 } from '../controllers/user.controller';
 import verifyAuthAdminMiddleware from '../middleware/verifyAuthAdmin.middleware';
 
 const userRoutes = Router();
 
 userRoutes.post('', verifyAuthAdminMiddleware, createUserController);
+userRoutes.patch('', verifyAuthAdminMiddleware, updateUserController);
 
 export default userRoutes;
