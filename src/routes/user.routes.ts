c60de0c4382456c9a0ecee05246804f7718d6dba
pedrolibas <102977943+pedrolibas@users.noreply.github.com>
2023-11-10 09:49:24 -0300

    feat: add user list route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import {
     createUserController,
     updateUserController,
-    profileUserController
+    profileUserController,
+    listUserController
 } from '../controllers/user.controller';
 import verifyAuthMiddleware from '../middleware/verifyAuth.middleware';
 import verifyAdmin from '../middleware/verifyAdmin.middleware';
@@ -12,5 +13,6 @@ const userRoutes = Router();
 userRoutes.post('', verifyAuthMiddleware, verifyAdmin, createUserController);
 userRoutes.patch('/:id', verifyAuthMiddleware, verifyAdmin, updateUserController);
 userRoutes.get('/profile', verifyAuthMiddleware, profileUserController);
+userRoutes.get('', verifyAuthMiddleware, verifyAdmin, listUserController);
 
 export default userRoutes;
