376724b716b3a23e48632639fee5180d4649aff6
pedrogv03@hotmail.com <email@email.com>
2023-11-12 04:24:13 -0300

    feat: create route guest delete

--- src/routes/guest.routes.ts
+++ src/routes/guest.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import verifyAuthMiddleware from "../middlewares/verifyAuth.middleware";
 import verifyConnected from "../middlewares/verifyConnected.middleware";
-import { createGuestController, listGuestController, updateGuestController } from "../controllers/guest.controller";
+import { createGuestController, deleteGuestController, listGuestController, updateGuestController } from "../controllers/guest.controller";
+import verifyAdmin from "../middlewares/verifyAdmin.middleware";
 
 const guestRoutes = Router();
 
 guestRoutes.post('', verifyAuthMiddleware, verifyConnected, createGuestController);
 guestRoutes.get('', verifyAuthMiddleware, verifyConnected, listGuestController);
 guestRoutes.patch('/:id', verifyAuthMiddleware, verifyConnected, updateGuestController);
+guestRoutes.delete('/:id', verifyAuthMiddleware, verifyAdmin, verifyConnected, deleteGuestController);
 
 export default guestRoutes;
