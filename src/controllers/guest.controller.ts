94e81785fee4d45241e35dc2e6d58a81c94d1039
pedrogv03@hotmail.com <email@email.com>
2023-11-12 04:04:33 -0300

    feat: create route list guest

--- src/controllers/guest.controller.ts
+++ src/controllers/guest.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import createGuestService from "../services/guest/createGuest.service";
+import listGuestService from "../services/guest/listGuest.service";
 
 export const createGuestController = async (req: Request, res: Response) => {
     const { name, phone_number, nationality, emergency_contact, cpf } = req.body;
@@ -8,3 +9,10 @@ export const createGuestController = async (req: Request, res: Response) => {
 
     return res.status(201).json(guestCreated);
 };
+
+export const listGuestController = async (req: Request, res: Response) => {
+    const guests = await listGuestService();
+
+    return res.status(200).json(guests);
+};
+
--- src/routes/guest.routes.ts
+++ src/routes/guest.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import verifyAuthMiddleware from "../middlewares/verifyAuth.middleware";
 import verifyConnected from "../middlewares/verifyConnected.middleware";
-import { createGuestController } from "../controllers/guest.controller";
+import { createGuestController, listGuestController } from "../controllers/guest.controller";
 
 const guestRoutes = Router();
 
 guestRoutes.post('', verifyAuthMiddleware, verifyConnected, createGuestController);
+guestRoutes.get('', verifyAuthMiddleware, verifyConnected, listGuestController);
 
 export default guestRoutes;
