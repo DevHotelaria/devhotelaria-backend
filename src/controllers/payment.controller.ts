b593c57ba8d045723c57e223ea17f3d55e6e2683
pedrogv03@hotmail.com <email@email.com>
2023-11-12 12:49:19 -0300

    feat: create paid route

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from "express";
 import discountPaymentService from "../services/payment/discountPayment.service";
+import paidPaymentService from "../services/payment/paidPayment.service";
 
 export const discountPaymentController = async (req: Request, res: Response) => {
     const { privileges, discount } = req.body;
     const id = req.params.id;
 
     const managerUpdated = await discountPaymentService({privileges, discount}, id);
+
+    return res.status(200).json(managerUpdated);
+}
+
+export const paidPaymentController = async (req: Request, res: Response) => {
+    const { value } = req.body;
+    const id = req.params.id;
+
+    const managerUpdated = await paidPaymentService(value, id);
 
     return res.status(200).json(managerUpdated);
 }
